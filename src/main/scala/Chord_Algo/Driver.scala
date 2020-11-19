d7bed7a2b1388f3a1ea270c74cdf1e55a2a6989b
Alexis Leonardo Jalomo <ajalom2@uic.edu>
2020-11-18 20:59:20 -0600

    done

--- src/main/scala/Chord_Algo/Driver.scala
+++ src/main/scala/Chord_Algo/Driver.scala
@@ -1,14 +1,33 @@
 package Chord_Algo
 
-import akka.actor.ActorSystem
-import akka.actor.typed.Props
-import com.typesafe.config.{Config, ConfigFactory}
+import akka.actor.typed.javadsl.Behaviors
+import akka.actor.typed.{ActorSystem, Behavior}
 
-class Driver {
+object Simulation {
   def main(args: Array[String]): Unit = {
-    val driver: ActorSystem = ActorSystem("Driver")
-    //val chord  = driver.actorOf(Props[Chord], name = "Chord")
-    //val config: Config = ConfigFactory.load()
-    //chord ! Chord.initializeNodesWithConfig(config, )
+    val system: ActorSystem[Driver.lookup] =
+      ActorSystem(Driver(), "driver")
+
+    system ! Driver.lookup("ToyStory")
+    system ! Driver.lookup("MoneyTrain")
   }
+}
+
+object Driver {
+
+  final case class lookup(key: String)
+
+  def apply(): Behavior[lookup] =
+    Behaviors.setup { context =>
+      val chord = context.spawn(Chord(), "chord")
+
+      Behaviors.receiveMessage { message =>
+        val replyTo = context.spawn(User(), message.key)
+        chord ! Chord.keyLookup("MoneyTrain", replyTo)
+        val value = Some(Chord.getChordActor.dictionary(message.key))
+        replyTo ! User.queryResponse(message.key, value)
+        Behaviors.same
+      }
+    }
+
 }
