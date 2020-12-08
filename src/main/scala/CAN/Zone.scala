d4ce63697c15fd2fec163273e15f0c31d094e8dd
Jacob Sanchez <jacobsanchez@Jacobs-MacBook-Pro.local>
2020-12-08 17:24:53 -0600

    Neighbors set up

--- src/main/scala/CAN/Zone.scala
+++ src/main/scala/CAN/Zone.scala
@@ -1,6 +1,7 @@
 package CAN
 
 import CAN.Zone.neighborTable
+import CAN.direction.{direction, left, up, right, down}
 import Chord_Algo.Hash
 import akka.actor.typed.ActorRef
 import com.typesafe.config.ConfigFactory
@@ -28,6 +29,34 @@ class Zone(X_range: (Double , Double), Y_range: (Double , Double)) {
   def splitZone(new_node: ActorRef[Node.Command]): Unit = {
 
   }
-  def set_neighbors(entry: Neighbor, i: Int): Unit =
-    neighborTable.neighbors(i) = entry
+  def set_neighbor(node: ActorRef[Node.Command], zone: Zone): Unit = {
+    val X_axis = zone.get_XRange
+    val Y_axis = zone.get_YRange
+    val x = this.get_XRange
+    val y = this.get_YRange
+    var direction: direction = left
+    var entry = Neighbor(node, (0, 0), direction)
+    // Same Zone
+    if(x == X_axis && y == Y_axis) return
+    /* Find Direction of neighbor */
+    if(X_axis._1 < x._1 && X_axis._2 == x._1 && Y_axis == y)
+      direction = left
+    else if(X_axis._1 ==  x._2 && X_axis._2 > x._1 && Y_axis == y)
+      direction = right
+    else if(X_axis == x && Y_axis._1 == y._2 && Y_axis._2 > y._2)
+      direction = up
+    else
+      direction = down
+    // Set Up Entry
+    if(direction == up || direction == down) {
+      entry = Neighbor(node, X_axis, direction)
+      if(direction == up)  neighborTable.neighbors(1) = entry
+      else neighborTable.neighbors(3) = entry
+    }
+    if(direction == left || direction == right) {
+      entry =  Neighbor(node, Y_axis, direction)
+      if(direction == left)  neighborTable.neighbors(0) = entry
+      else neighborTable.neighbors(2) = entry
+    }
+  }
 }
