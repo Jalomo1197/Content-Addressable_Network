1aada7961d40f3ecde87eb8c9565c6f345680ea1
Jacob Sanchez <jacobsanchez@Jacobs-MacBook-Pro.local>
2020-12-08 22:34:00 -0600

    visited

--- src/main/scala/CAN/Neighbor.scala
+++ src/main/scala/CAN/Neighbor.scala
@@ -1,19 +1,21 @@
 package CAN
 
-import CAN.direction.direction
 import akka.actor.typed.ActorRef
 
 object Neighbor{
-  def apply(node: ActorRef[Node.Command], range: (Double, Double), direction: direction): Neighbor = new Neighbor(node, range, direction)
+  def apply(node: ActorRef[Node.Command], range: (Double, Double), direction: Zone.direction): Neighbor = new Neighbor(node, range, direction)
 }
-class Neighbor(var node: ActorRef[Node.Command], var range: (Double, Double), var direction: direction) {
+class Neighbor(var node: ActorRef[Node.Command], var range: (Double, Double), var direction: Zone.direction) {
+  var visited = false
+  def setVisited(): Unit =
+    this.visited = true
   def setNode(node: ActorRef[Node.Command]): Unit =
     this.node = node
   def getNode: ActorRef[Node.Command] =
     this.node
   def getRange: (Double, Double) =
     this.range
-  def getDirection: direction =
+  def getDirection: Zone.direction =
     this.direction
   def setRange(range: (Double, Double)): Unit =
     this.range = range
